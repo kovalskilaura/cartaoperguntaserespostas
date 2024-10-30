cbc8d83ef080f107b5abd8fda25a39a045f07c36
kovalskilaura <144070956+kovalskilaura@users.noreply.github.com>
2024-10-29 21:29:11 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,137 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Capitais',
+    'Qual a capital de Santa Catarina',
+    'Florianópolis'
+)
+
+
+criaCartao(
+    'Capitais',
+    'Qual a capital do Paraná?',
+    'Curitiba'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Capitais',
+    'Qual a capital de São Paulo?',
+    'São Paulo'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Capitais',
+    'Qual a capital do Rio de Janeiro?',
+    'Rio de Janeiro'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Capitais',
+    'Qual a capital de Minas Gerais'?',
+    'Belo Horizonte'
+
 )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital de Espirito Santo?',
+    'Vitória'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital de Mato Grosso do Sul?',
+    'Campo Grande'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital de Mato Grosso do Norte?',
+    'Cuiabá'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital de Goiás?',
+    'Brasilia e Goiânia'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital da Bhaia?',
+    'Salvador'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital Sergipe?',
+    'Aracaju'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital de Alagoas?',
+    'Maceió'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital de Pernambuco?',
+    'Recife'
+
+    )
+criaCartao(
+    'Capitais',
+    'Qual a Capital da Paraíba?',
+    'João Pessoa'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital do Rio Grande do Norte?',
+    'Natal'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital do Ceará?',
+    'Fortaleza'
+
+    )
+
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital do Píaui?',
+    'Teresina'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital do Maranhão?',
+    'São Luís'
+
+    )
+
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital do Pará?',
+    'Belém'
+
+    )
+
+criaCartao(
+    'Capitais',
+    'Qual a Capital Espirito Santo?',
+    'Vitória'
+
+    )
